cc20cee9ff72ec08ffc8dcf4e5672d43e4c6fd1c
elmirarokni <104225186+elmirarokni@users.noreply.github.com>
2022-05-30 00:47:13 +0430

    Create targetAccounts.js

---
+++ targetAccounts.js
@@ -0,0 +1,108 @@
+const targetAccounts = [
+
+
+	///////////POPULAR DELRAY LOCAL PLACES
+	'https://www.instagram.com/new_vegan76/',
+	'https://www.instagram.com/restorationlane/',
+	'https://www.instagram.com/delraymag/',
+	'https://www.instagram.com/renovatewiththeroots/',
+	'https://www.instagram.com/mmwooddesigns/',
+	'https://www.instagram.com/sassafraswpb/',
+	'https://www.instagram.com/realtordotcom/',
+	'https://www.instagram.com/sloans_southflorida/',
+	'https://www.instagram.com/bocalifemag/',
+	'https://www.instagram.com/oldschoolsquaredelray/',
+	'https://www.instagram.com/serenitynowstaging/',
+	'https://www.instagram.com/himmelsellshomes',
+	'https://www.instagram.com/cerverare',
+	'https://www.instagram.com/livingproof.photography/',
+	'https://www.instagram.com/megangribblehome',
+	'https://www.instagram.com/jillszedergables/',
+	'https://www.instagram.com/flippingahouse/',
+	'https://www.instagram.com/landscapeartinc/',
+	'https://www.instagram.com/rncbuilders/',
+	'https://www.instagram.com/roccostacos/',
+	'https://www.instagram.com/delraybeachmarket/',
+	'https://www.instagram.com/bodegataqueria/',
+	'https://www.instagram.com/thegrovedelrayfl/',
+	'https://www.instagram.com/cut432steakhouse/',
+	'https://www.instagram.com/theheartofdelraygallery/',
+	'https://www.instagram.com/bluefineart/',
+	'https://www.instagram.com/artsgaragedelraybeach/',
+	'https://www.instagram.com/cornellartmuseum/',
+	'https://www.instagram.com/dadadelray/',
+	'https://www.instagram.com/elcaminofla' /
+	'https://www.instagram.com/lesorellerestaurant/',
+	'https://www.instagram.com/deck84_delray/',
+	'https://www.instagram.com/parktavernfl/',
+	'https://www.instagram.com/brule.bistro/',
+	'https://www.instagram.com/habitatforhumanity/',
+	'https://www.instagram.com/bocaratonobserver/',
+	'https://www.instagram.com/delraybeachgolfclub/',
+	'https://www.instagram.com/puttnaround/',
+	'https://www.instagram.com/sandowaydiscovery/',
+	'https://www.instagram.com/sundy_house/',
+
+
+];
+
+module.exports = targetAccounts;
+
+
+/*  PAGES WOMEN GO TO
+	'https://www.instagram.com/boandtee/',
+	'https://www.instagram.com/revolve/',
+	'https://www.instagram.com/revolvebeauty/',
+	'https://www.instagram.com/maccosmetics/',
+	'https://www.instagram.com/ohpolly/',
+	'https://www.instagram.com/ohpollyswim/',
+	'https://www.instagram.com/pallascouture/',
+	'https://www.instagram.com/pallascollective/',
+	'https://www.instagram.com/studio.albus/',
+	'https://www.instagram.com/seduction_la/',
+	'https://www.instagram.com/fuckologyofficial/',
+	'https://www.instagram.com/betchwithnolife/',
+	'https://www.instagram.com/basic/',
+	'https://www.instagram.com/bipolar_girlfriend_/',
+	'https://www.instagram.com/ohmboutique/',
+	'https://www.instagram.com/yourbreakupbestie/',
+	'https://www.instagram.com/sahirajewelrydesign/',
+	'https://www.instagram.com/okb_int/',
+	'https://www.instagram.com/cottononbody/',
+	'https://www.instagram.com/lv_perfection_medspa/',
+	'https://www.instagram.com/berrybeachyswimwear/',
+	'https://www.instagram.com/callherdaddy/',
+	'https://www.instagram.com/bangn.body/',
+	'https://www.instagram.com/lovetrends.shop/',
+	'https://www.instagram.com/glossy_zodiac/',
+	'https://www.instagram.com/bossbabe.inc/',
+	*/
+
+
+/*
+
+
+
+*/
+
+/*LIST TARGETING MEN - ONLYFANS
+	'https://www.instagram.com/mandi_lynn_bbw/',
+	'https://www.instagram.com/therealnatashatosini/',
+	'https://www.instagram.com/jaidexoxo_/',
+	'https://www.instagram.com/ivykiaraofficial/',
+	'https://www.instagram.com/tatted.mama44/',
+	'https://www.instagram.com/officialmakaylaalt/',
+	'https://www.instagram.com/amberleighhh/',
+	'https://www.instagram.com/devin.meow/',
+	'https://www.instagram.com/jessicakes33/',
+	'https://www.instagram.com/jessicacakes2/',
+	'https://www.instagram.com/jessicafitcake/',
+	'https://www.instagram.com/millenawhite/',
+	'https://www.instagram.com/_lucky_ladybird/',
+	'https://www.instagram.com/millena_white/',
+	'https://www.instagram.com/missjessarhodes/',
+	'https://www.instagram.com/fierysireniv/',
+	'https://www.instagram.com/egoistqaa/',
+	'https://www.instagram.com/liubov_kalish/',
+	'https://www.instagram.com/lsdiamonds69/',
+	*/
